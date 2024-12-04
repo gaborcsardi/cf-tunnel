5b244b50934bdc3b2934b9f68df5a06f26c72676
Gábor Csárdi <csardi.gabor@gmail.com>
2024-12-04 23:47:09 +0100

    Install newer version, simplify output

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ const fs = require("fs");
 const path = require("path");
 const os = require("os");
 
-
 async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-
 async function download() {
-  NGROK_MAC   = "https://github.com/cloudflare/cloudflared/releases/download/2022.10.3/cloudflared-darwin-amd64.tgz"
-  NGROK_Linux = "https://github.com/cloudflare/cloudflared/releases/download/2022.10.3/cloudflared-linux-amd64"
-  NGROK_Win   = "https://github.com/cloudflare/cloudflared/releases/download/2022.10.3/cloudflared-windows-amd64.exe"
-
+  NGROK_MAC   = "https://github.com/cloudflare/cloudflared/releases/download/2024.11.1/cloudflared-darwin-amd64.tgz"
+  NGROK_Linux = "https://github.com/cloudflare/cloudflared/releases/download/2024.11.1/cloudflared-linux-amd64"
+  NGROK_Win   = "https://github.com/cloudflare/cloudflared/releases/download/2024.11.1/cloudflared-windows-amd64.exe"
 
   let link = NGROK_Win;
   let ext = "";
@@ -27,12 +24,9 @@ async function download() {
     link = NGROK_Linux;
   }
 
-
   let workingDir = __dirname;
   {
-    core.info("Downloading: " + link);
     let img = await tc.downloadTool(link);
-    core.info("Downloaded file: " + img);
     
     if (os.platform() == "darwin") {
       await io.mv(img, path.join(workingDir, "./cf." + ext));
@@ -44,20 +38,7 @@ async function download() {
     } else {
       await io.mv(img, path.join(workingDir, "./cloudflared.exe"));
     }
-  
-  
-
   }
-
-//  if (ext === "tgz") {
-//    await exec.exec("tar -xzf " + path.join(workingDir, "./cf." + ext));
-//    await io.mv("cloudflared", path.join(workingDir, "cloudflared"));
-//  } else if (link === NGROK_MAC) {
-//    await exec.exec("7za e -y " + path.join(workingDir, "./cf." + ext) + "  -o" + workingDir);
-//  } else {
-//    await exec.exec("unzip " + path.join(workingDir, "./cf." + ext) + "  -d " + workingDir);
-//  }
-
 }
 
 async function run(protocol, port) {
@@ -71,19 +52,17 @@ async function run(protocol, port) {
     log = log.replace(/^(\w):|\\+/g,'/$1');
   }
 
-  await exec.exec("sh", [], { input: `if ! ${cfd} update; then echo ok;fi` });
+  await exec.exec("sh", [], { input: `if ! ${cfd} update; then true; fi` });
   await exec.exec("sh", [], { input: `${cfd} tunnel --url ${protocol}://localhost:${port} >${log} 2>&1 &` });
 
-
-  for (let i = 0; i < 10; i++) {
-    await sleep(5000);
+  for (let i = 0; i < 50; i++) {
+    await sleep(1000);
     let output = "";
     await exec.exec("sh", [], {
       input: `cat "${log}" | grep https:// | grep trycloudflare.com | head -1 | cut -d '|' -f 2 | tr -d ' ' | cut -d '/' -f 3`,
       listeners: {
         stdout: (s) => {
           output += s;
-          core.info(s);
         }
       }
     });
@@ -91,42 +70,30 @@ async function run(protocol, port) {
     if (!server) {
       continue;
     }
-    core.info("server: " + server);
     await exec.exec("sh", [], { input: `echo "server=${server}" >> $GITHUB_OUTPUT` });
 
     break;
   }
-
-
 }
-
-
 
 async function main() {
 
   let protocol = core.getInput("protocol");
-  core.info("protocol: " + protocol);
   if (!protocol) {
     protocol = "tcp";
   }
 
   let port = core.getInput("port");
-  core.info("port: " + port);
   if (!port) {
     core.setFailed("No port !");
     return;
   }
 
-
   await download();
-
   await run(protocol, port);
-
 
   process.exit();
 }
-
-
 
 main().catch(ex => {
   core.setFailed(ex.message);
